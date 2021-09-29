cc1af6b075957a6d21d6571f08b5400c0047c3b1
Laverdure77 <meulemansph@hotmail.com>
2021-09-29 17:10:50 +0200

    Dom 1

--- 1.Introduction/script.js
+++ 1.Introduction/script.js
@@ -1,2 +1,13 @@
 /*
 */
+/* console.log(document.title); */
+var x = document.title;
+console.log(x);
+document.title = "Modifying the DOM";
+
+var c = document.body.children;
+  var txt = "";
+  var i;
+  for (i = 0; i < c.length; i++) {
+    txt = txt + c[i].tagName + "<br>";
+  }
