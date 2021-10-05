85806061f826ec0c689618c25d5565bb46ea3d0c
Laverdure77 <meulemansph@hotmail.com>
2021-10-05 10:22:14 +0200

    5.events

--- 3.Create/script.js
+++ 3.Create/script.js
@@ -1,2 +1,113 @@
 /*
 */
+
+/* let section = document.createElement('section');
+section.innerHTML = ' learner name ';
+article.appendChild( section ); */
+const beCodeStudent = [
+    {
+        id: 1,
+        firstname: "Antoine",
+        lastname: "Ghysens"
+    },
+    {
+        id: 2,
+        firstname: "Baptiste",
+        lastname: "Geron"
+    },
+    {
+        id: 3,
+        firstname: "Bathsheba",
+        lastname: "Deepijan"
+    },
+    {
+        id: 4,
+        firstname: "Bruno",
+        lastname: "Presti"
+    },
+    {
+        id: 5,
+        firstname: "",
+        lastname: ""
+    },
+    {
+        id: 6,
+        firstname: "Fabrice",
+        lastname: "Castrogiovanni"
+    },
+    {
+        id: 7,
+        firstname: "Fanny",
+        lastname: "Fraiture"
+    },
+    {
+        id: 8,
+        firstname: "Francis",
+        lastname: "FranÃ§ois"
+    },
+    {
+        id: 9,
+        firstname: "Frederic",
+        lastname: "Van Overmeire"
+    },
+    {
+        id: 10,
+        firstname: "JC",
+        lastname: "Molhant"
+    },
+    {
+        id: 11,
+        firstname: "Maxim",
+        lastname: "K."
+    },
+    {
+        id: 12,
+        firstname: "Michael",
+        lastname: "Tesfay"
+    },
+    {
+        id: 13,
+        firstname: "Philippe",
+        lastname: "Meulemans"
+    },
+    {
+        id: 14,
+        firstname: "Raoni",
+        lastname: "Gillet"
+    },
+    {
+        id: 15,
+        firstname: "Stephane",
+        lastname: "Genet"
+    },
+    {
+        id: 16,
+        firstname: "Thomas",
+        lastname: "Backers"
+    },
+
+] 
+
+beCodeStudent.forEach(element => {
+    
+    //Random Color
+    let r = Math.floor( (Math.random()*255));
+    let g = Math.floor( (Math.random()*255));
+    let b = Math.floor( (Math.random()*255));
+    let rdmcolor = 'rgb('+r+','+g+','+b+')';
+    let dl = 100;
+    if ((r<dl)&&(g<dl)&&(b<dl)) {
+        let tcolor = "white";
+    }
+    else{
+        tcolor= "black"
+    }
+    // paragraphe for each Object in the beCodeStudent Array
+    let p = document.createElement("p");
+    p.innerText= `${element.firstname} ${element.lastname}`;
+    p.style.backgroundColor = rdmcolor;
+    p.style.color = "tcolor";
+    let article = document.querySelector('article');
+    article.appendChild(p);
+        
+});
--- 5.Events/script.js
+++ 5.Events/script.js
@@ -5,12 +5,146 @@ function getElapsedTime(){
 }
 
 function clickOnSquare(e){
+
   console.log(e.target.classList[1])
   console.log(getElapsedTime())
+  
 }
+
 
 const actionsquares = document.querySelectorAll('.actionsquare')
 for(let actionsquare of actionsquares){
   actionsquare.addEventListener('click', clickOnSquare)
 }
+
+
+
+//  function to create a new square
+function newSquare (squareColor){
+  
+  let divSQR = document.createElement('div');
+  divSQR.classList.add('displayedsquare');
+  divSQR.style.backgroundColor = squareColor;
+  let squareSectionParent = document.querySelector('.displayedsquare-wrapper');
+  squareSectionParent.appendChild(divSQR);
+  setListener(divSQR, squareColor);
+
+}
+
+// function to add click listener on each new square, and alert color
+
+function setListener( el, clr ) {
+            // Dynamic elements event listener
+            el.addEventListener( 'click', function() {
+                alert( 'Square color is ' +clr);
+            } );
+          }
+
+//Random color
+
+
+
+
+// function to create a new li with log
+function newLog (y) {
+  
+  let timeSQR = getElapsedTime();
+  /* let type = y.target.classList[1]; */
+  let liSQR = document.createElement('li');
+  
+  liSQR.innerText = '['+ timeSQR+'] Created a new ' + y +' square';
+  let logList = document.querySelector('ul');
+  logList.appendChild(liSQR);
+}
+
+
+//Targeting each square by color
+
+let actionSquare = document.querySelectorAll('.actionsquare-wrapper');
+/* console.log(actionSquare); */
+let allSquare = actionSquare[0].children;
+let greenSquare = allSquare[0];
+let violetSquare = allSquare[1];
+let orangeSquare = allSquare[2];
+/* console.log( greenSquare) */
+
+
+// event listener for each static square
+
+
+
+greenSquare.addEventListener( 'click', ()=>(
+  newSquare( 'lime' ),
+  newLog ( 'lime ')
+))
+
+violetSquare.addEventListener( 'click', ()=>(
+  newSquare( 'magenta'),
+  newLog ('magenta')
+))
+
+orangeSquare.addEventListener( 'click', ()=>(
+  newSquare('orange'),
+  newLog ('orange')
+
+))
+
+// Random body background color
+
+let body = document.querySelector( 'body');
+
+function bGColor() {
+  let r = Math.floor( (Math.random()*255));
+  let g = Math.floor( (Math.random()*255));
+  let b = Math.floor( (Math.random()*255));
+  let rdmcolor = 'rgb('+r+','+g+','+b+')';
+  body.style.backgroundColor = rdmcolor
+} 
+
+// delete log
+
+function delLog() {
+
+  let ul = document.querySelector('ul');
+  console.log(ul)
+  let sectDel= ul.children[0];
+  console.log(sectDel);
+  ul.removeChild(sectDel);
+}
+// delete generated square
+
+function delSquare(){
+
+ let dispSQR = document.querySelector(".displayedsquare-wrapper");
+ let delSQR = dispSQR.children[0];
+ dispSQR.removeChild(delSQR);
+
+}
+
+// event listener on SPACEBAR, LKEY, SKEY.
+
+body.addEventListener( 'keydown', (e)=>{
+  console.log(e.key )
+  console.log(e)
+  if (e.key == ' ') {
+    bGColor() 
+  }
+})  
+
+body.addEventListener( 'keydown', (e)=>{
+  console.log(e.key)
+  console.log(e)
+  if (e.key == 'l' ) {
+    delLog()
+  }
+})  
+
+body.addEventListener( 'keydown', (e)=>{
+  console.log(e.key)
+  console.log(e)
+  if (e.key == 's' ) {
+    delSquare()
+  }
+})  
+
 
